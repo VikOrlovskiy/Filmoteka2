10854da42002aebb2794634c12f4abcec057adf5
vik_orel <vikorel34@gmail.com>
2021-12-27 21:39:24 +0200

    Update apiServiceSearch.js

--- src/js/apiServiceSearch.js
+++ src/js/apiServiceSearch.js
@@ -4,29 +4,32 @@ const KEY ='api_key=3bcb9c362d567dc87ce4f986b1e52bec'
 const axios = require('axios');
 // ==================================================
 export default class DataFetch {
-    constructor(){
-        this.searchValue = '';
-        this.language ='en-US';
-        this.filmForAdult = false;
-        this.moveID = 0;
-        this.genres=[];
-        this.page = 1;
-        this.totalPages = 1;
+constructor(){
+    this.searchValue = '';
+    this.language ='en-US';
+    this.filmForAdult = false;
+    this.moveID = 0;
+    this.genres=[];
+    this.page = 1;
+    this.totalPages = 1;
     }
-    // ================== GET top film ===============  
-    fetchTopFilms() {
-         const response = await axios.get(`${URL}trending/movie/week?${KEY}&page=${this.page}`)
-         return response.data} 
-        // ============= GET film by user value ===============  
-    fetchFilms() {
-         const response = await axios.get(`${URL}search/movie?${KEY}&language=${this.language}&query=${this.value}&page=${this.page}&include_adult=${this.filmForAdult}`)
-         return response.data} 
-       // ============= GET FILM GENRE ===============  
-    fetchGenres() {
-         const response = await axios.get(`${URL}/genre/movie/list?${KEY}&language=${this.language}`)
-         this.genres = response.data.genres}
-       // ============= GET FILM by id ===============  
-    fetchFilmByID() {
-         const response = await axios.get(`${URL}/movie/${this.moveID}?${KEY}&language=${this.language}`)
-         return response.data} 
+// ============= GET FILM GENRE ===============  
+fetchGenres() {
+    return axios.get(`${URL}/genre/movie/list?${KEY}&language=${this.language}`).then(response=> {
+     console.log(this.genres )   
+     this.genres = response.data.genres})}
+// ================== GET top film ===============  
+fetchTopFilms() {
+   return axios.get(`${URL}trending/movie/week?${KEY}&page=${this.page}`).then(films => films.data)} 
+// ============= GET film by user value ===============  
+fetchFilms() {
+   return axios.get(`${URL}search/movie?${KEY}&language=${this.language}&query=${this.value}&page=${this.page}&include_adult=${this.filmForAdult}`).then(response => response.data)} 
+ // ============= GET FILM by id ===============  
+fetchFilmByID() {
+   return axios.get(`${URL}/movie/${this.moveID}?${KEY}&language=${this.language}`).then(response => response.data)} 
+// =================== get/set=======================
+    get query() {return this.searchQuery}
+    set query(newQuery) {this.searchQuery = newQuery}
+    get pageNum() {return this.page}
+    set pageNum(newPage) {this.page = newPage}
 }
