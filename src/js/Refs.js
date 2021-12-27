6855c70d5e12489d8390d604a4a5e7bf6ea7a695
vik_orel <vikorel34@gmail.com>
2021-12-27 15:24:11 +0200

    Create Refs.js

---
+++ src/js/Refs.js
@@ -0,0 +1,29 @@
+export default  {
+    // =====================header==================================
+    headerContainer:document.querySelector('.container-header'),
+    // =====================sitename===============================
+    backHomePage:document.querySelector('.name'),
+    // =====================navigation panel=======================
+    navigationPanel: document.querySelector('.navigation'),
+    homeButton: document.querySelector('[HOME]'),
+    myGalleryButton: document.querySelector('[LIBRARY]'),
+    userButtons:document.querySelector('.header-button-block'),
+    // ===============search==============
+    searchBlock: document.querySelector('.search'),
+    searchForm: document.querySelector('#search'),
+    searchButton: document.querySelector('.searching_button'),
+    searchInput: document.querySelector('.search_input'),
+    errorTextField:document.querySelector('.search_text'),
+    // =====================main====================================
+    galleryRef:document.getElementById('gallery'),
+     // =====================pagination=============================
+    paginationList:document.querySelector('.pagination_list'),
+    lastPagePagination:document.querySelector('.last-page'),
+    // =======================modal=================================
+    // backDrop: document.querySelector('.back-drop'),
+    // modalWindow: document.querySelector('.modal'),
+    // =======================footer================================
+    // backDrop: document.querySelector('.back-drop'),
+    // modalWindow: document.querySelector('.modal'),
+  };
+  
