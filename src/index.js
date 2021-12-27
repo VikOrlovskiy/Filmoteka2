38589b3d706523f6b1410e5d18cfcc000e06b5e4
vik_orel <vikorel34@gmail.com>
2021-12-27 14:23:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+// ==========styles=================
 import './sass/main.scss';
+// ==========Document Refs===========
+import "./js/Refs";
+// ==========Document HEADER=========
+import "./js/header";
+// ==========Document FOOTER=========
+import "./js/footer";
