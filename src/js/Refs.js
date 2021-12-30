05079e5481c70efb7e4933cf9e3cd20358589d82
vik_orel <vikorel34@gmail.com>
2021-12-30 22:33:57 +0200

    Update Refs.js

--- src/js/Refs.js
+++ src/js/Refs.js
@@ -1,5 +1,5 @@
 export default  {
-    // =====================header==================================
+    // =====================header=================================
     headerContainer:document.querySelector('.container-header'),
     // =====================sitename===============================
     backHomePage:document.querySelector('.name'),
@@ -8,7 +8,7 @@ export default  {
     homeButton: document.querySelector('[HOME]'),
     myGalleryButton: document.querySelector('[LIBRARY]'),
     userButtons:document.querySelector('.header-button-block'),
-    // ===============search==============
+    // ===============search=======================================
     searchBlock: document.querySelector('.search'),
     searchForm: document.querySelector('#search'),
     searchButton: document.querySelector('.searching_button'),
@@ -16,16 +16,26 @@ export default  {
     errorTextField:document.querySelector('.search_text'),
     // =====================main====================================
     galleryRef:document.getElementById('gallery'),
-     // =====================pagination=============================
-    // paginationList:document.querySelector('.pagination_list'),
-    // lastPagePagination:document.querySelector('.last-page'),
     // =======================modal=================================
     backDrop: document.querySelector('.back-drop'),
     modalWindow: document.querySelector('.modal'),
     productCardInWindow: document.querySelector('.product'),
     modalButtonClose:document.querySelector('.modal_close'),
+    // =======================pagination============================
+    paginationList:document.querySelector('.pagination_list'),
+    paginationListButtonPrevPage:document.querySelector('.prewPage'),
+    firstPagePagination:document.querySelector('.first-page'),
+    prevPageNumber:document.querySelector('.number.prewPage'),
+    prevPageDots:document.querySelector('.dots.prewPage'),
+    activePaginationPage:document.querySelector('.number.active'),
+    nextPageNumber:document.querySelector('.number.nextPage'),
+    nextPageDots:document.querySelector('.dots.nextPage'),
+    lastPagePagination:document.querySelector('.last-page'),
+    paginationListButtonNextPage:document.querySelector('.pagination_item.nextPage'),
+
     // =======================footer================================
     // backDrop: document.querySelector('.back-drop'),
     // modalWindow: document.querySelector('.modal'),
+
   };
   
