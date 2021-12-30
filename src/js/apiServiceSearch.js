9e9e2077cf9f72f66055aad698c18068bd853537
vik_orel <vikorel34@gmail.com>
2021-12-30 22:33:26 +0200

    Update apiServiceSearch.js

--- src/js/apiServiceSearch.js
+++ src/js/apiServiceSearch.js
@@ -4,6 +4,13 @@ const KEY ='api_key=3bcb9c362d567dc87ce4f986b1e52bec'
 const axios = require('axios');
 // ==================================================
 export default class DataFetch {
+// ============= GET FILM GENRE ===============  
+static fetchGenres() {
+        return axios.get(`${URL}/genre/movie/list?${KEY}&language=${this.language}`)
+    .then(response=> {this.genres = response.data.genres})}
+// ============= GET film by ID ===============
+    static  fetchFilmByID() {
+        return axios.get(`${URL}/movie/${this.moveID}?${KEY}&language=${this.language}`).then(response => response.data)} ;
 constructor(){
     this.searchValue = '';
     this.language ='en-US';
@@ -11,21 +18,19 @@ constructor(){
     this.moveID = 0;
     this.genres=[];
     this.page = 1;
-    this.totalPages = 1;
+    this.totalPages = 99;
+    console.log(this.page)
     }
-// ============= GET FILM GENRE ===============  
-fetchGenres() {
-    return axios.get(`${URL}/genre/movie/list?${KEY}&language=${this.language}`)
-    .then(response=> {this.genres = response.data.genres})}
-// ================== GET top film ===============  
-fetchTopFilms() {
-   return axios.get(`${URL}trending/movie/week?${KEY}&page=${this.page}`).then(films => films.data)} 
 // ============= GET film by user value ===============  
 fetchFilms() {
-   return axios.get(`${URL}search/movie?${KEY}&language=${this.language}&query=${this.value}&page=${this.page}&include_adult=${this.filmForAdult}`).then(response => response.data)} 
- // ============= GET FILM by id ===============  
-fetchFilmByID() {
-   return axios.get(`${URL}/movie/${this.moveID}?${KEY}&language=${this.language}`).then(response => response.data)} 
+   return axios.get(`${URL}search/movie?${KEY}&language=${this.language}&query=${this.searchValue}&page=${this.page}&include_adult=${this.filmForAdult}`).then(response => response.data)} 
+// ================== GET top film ===============  
+fetchTopFilms() {
+    return axios.get(`${URL}trending/movie/week?${KEY}&page=${this.page}`).then(films => {return  films.data})} 
+// =============Pagination Actions===============  
+   nextPage(){this.page +=1;}
+   prewPage(){this.page -=1;}
+   pageReset(){this.page = 1;}
 // =================== get/set=======================
     get query() {return this.searchQuery}
     set query(newQuery) {this.searchQuery = newQuery}
