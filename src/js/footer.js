56698238db43fd74491acd5ba59301ca2964fe83
vik_orel <vikorel34@gmail.com>
2022-01-03 23:42:04 +0200

    Update footer.js

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,2 +1,18 @@
 import Refs from "./Refs";
-// console.log(Refs)
+import {onPressEscapeModalClose,onClickBackdropModalClose,onClickButtonModalClose} from "./closeModalFunctions";
+import devInfo from "./teamInform.js"
+import devCard from '../templates/devCard.hbs';
+Refs.footerButtonTeamModal.addEventListener('click',onClickOpenTeamModal)
+function onClickOpenTeamModal(e){
+    Refs.productCardInWindow.innerHTML = '';
+    renderFilmsCard(devInfo)
+    Refs.backDrop.classList.toggle('is-hidden')
+        // ============close modal
+        onClickButtonModalClose()
+        onClickBackdropModalClose()
+        onPressEscapeModalClose()
+    }
+console.log(devInfo)
+function renderFilmsCard(data){
+    return data.map(({name , icon , role }) => {Refs.productCardInWindow.insertAdjacentHTML('afterbegin',devCard({name , icon , role }))})
+}
