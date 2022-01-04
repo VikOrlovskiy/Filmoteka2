cb4b794b2c91aa5943c193d354e653baf024df04
vik_orel <vikorel34@gmail.com>
2022-01-04 20:14:45 +0200

    Update closeModalFunctions.js

--- src/js/closeModalFunctions.js
+++ src/js/closeModalFunctions.js
@@ -1,19 +1,32 @@
 import Refs from "./Refs";
-
+//============= modal close =================
+function onModalClose(){
+  onClickButtonModalClose()
+  onClickBackdropModalClose()
+  onPressEscapeModalClose()
+}
+// ============ close madal by esc tab btn ==
 function onPressEscapeModalClose(){
     document.addEventListener("keydown", e => {
-      if(e.key === "Escape"){
-        Refs.backDrop.classList.add('is-hidden')
-      }
+      if(e.key !== "Escape"){return}
+      Refs.backDrop.classList.add('is-hidden')
+      Refs.body.classList.remove("modal-open")
     })
   }
+  // ======== close madal by backdrop click ==
   function onClickBackdropModalClose(){
     Refs.backDrop.addEventListener('click', e => {
       if(e.target !== e.currentTarget){return}
       Refs.backDrop.classList.add('is-hidden')
+      Refs.body.classList.remove("modal-open")
     })
   }
+    // ====== close madal by close btn click ==
   function onClickButtonModalClose(){
-    Refs.modalButtonClose.addEventListener('click', e => Refs.backDrop.classList.add('is-hidden'))
+    Refs.modalButtonClose.addEventListener('click', e => {
+    Refs.body.classList.remove("modal-open")
+    Refs.backDrop.classList.add('is-hidden')})
   }
-  export{onPressEscapeModalClose,onClickBackdropModalClose,onClickButtonModalClose}
+
+
+  export{onModalClose}
