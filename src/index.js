036815ab5ceeff637e46fc8a790eae869e6866ad
vik_orel <vikorel34@gmail.com>
2022-01-13 16:27:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,28 +4,10 @@ import './sass/main.scss';
 import "./js/Refs";
 // ==========Document HEADER=========
 import "./js/header";
+import "./js/navigation";
+// ==========Document Auth=========
+import "./js/fireBaseApi";
 // ==========Document MAIN========
 import "./js/gallery";
 // ==========Document FOOTER=========
 import "./js/footer";
-//+++++++++++++++++++ filmoteca2 ++++++++++++++++++++++++++++ 
-import { initializeApp } from "firebase/app";
-import { getAuth,createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBR83s7HPzADcrtRoUE2ndSXGar5JAgfWk",
-  authDomain: "filmoteka2-11906.firebaseapp.com",
-  projectId: "filmoteka2-11906",
-  storageBucket: "filmoteka2-11906.appspot.com",
-  messagingSenderId: "12870537930",
-  appId: "1:12870537930:web:d7a02c2d3d10fd4e9f5a33"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
-const auth = getAuth(app)
-console.log(app)
-console.log(auth)
-console.log(db)
